a22c2455aed331d4a36e89938fcbcc2a5b62ddd5
Iván Jackson <ivan.jackson@upr.edu>
2019-03-11 19:19:41 -0400

    Finished Part 2

--- Stacks_Lab_Students/src/classes/SymmetricStringAnalyzer.java
+++ Stacks_Lab_Students/src/classes/SymmetricStringAnalyzer.java
@@ -12,6 +12,7 @@ public class SymmetricStringAnalyzer {
 	 */
 	public boolean isValidContent() {  
 		SLLStack<Character> stack = new SLLStack<Character>();
+		if(s.length()%2!=0) return false;
 		for (int i=0; i < s.length(); i++) { 
 	        char c = s.charAt(i); 
 	        if (Character.isLetter(c))
@@ -38,12 +39,48 @@ public class SymmetricStringAnalyzer {
 		return s; 
 	}
 
-	public String parenthesizedExpression() 
-	throws StringIsNotSymmetricException 
-	{
-		// ADD MISSING CODE
-		
-		return null;  // need to change if necessary....
+//	public String parenthesizedExpression() throws StringIsNotSymmetricException {
+//		if(!isValidContent()) 
+//			throw new StringIsNotSymmetricException("The string is not symetric");
+//		String str ="";
+//		SLLStack<String> stack = new SLLStack<String>();
+//		for (int i=0; i < s.length(); i++) { 
+//	        char c = s.charAt(i); 
+//	        if (Character.isLetter(c))
+//	           if (Character.isUpperCase(c)) { stack.push("<"+c+" "); str += stack.top();}
+//               else {
+//            	   char t = stack.top().charAt(1);
+//            	   if(t=='>')t = stack.top().charAt(0);
+//                 if (t == Character.toUpperCase(c)) {
+//                	 str += stack.top();
+//                	 stack.pop(); 
+//                	 
+//                	 }  
+//           		}
+//	        
+//		}
+//		return str;
+//	}
+	public String parenthesizedExpression() throws StringIsNotSymmetricException {
+		if(!isValidContent()) 
+			throw new StringIsNotSymmetricException("The string is not symetric");
+		String str ="";
+		SLLStack<String> stack = new SLLStack<String>();
+		for (int i=0; i < s.length(); i++) { 
+	        char c = s.charAt(i); 
+	        if (Character.isLetter(c))
+	           if (Character.isUpperCase(c)) { stack.push("<"+c+" "); str += stack.top();}
+               else {
+            	   char t = stack.top().charAt(1);
+            	   if(t=='>')t = stack.top().charAt(0);
+                 if (t == Character.toUpperCase(c)) {
+                	 str += c+"> ";
+                	 stack.pop(); 
+                	 
+                	 }  
+           		}
+	        
+		}
+		return str;
 	}
-
 }
